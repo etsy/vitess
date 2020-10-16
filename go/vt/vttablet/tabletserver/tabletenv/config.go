cb84cece2a549f577da9bed4d7bbbef283cb9819
Adam Saponara <asaponara@etsy.com>
2020-10-16 16:35:20 +0000

    [PATCH] truncate_error.patch
    adds -queryserver-config-truncate-error-len flag to vttablet to truncate error messages
    necessary because PDO cannot handle error messages over a certain length.
    
    See the following links for more details:
    - https://jira.etsycorp.com/browse/CORE-5828
    - https://jira.etsycorp.com/browse/CORE-6288
    - https://bugs.php.net/bug.php?id=78797
    
    Signed-off-by: mstarr <mstarr@etsy.com>

--- go/vt/vttablet/tabletserver/tabletenv/config.go
+++ go/vt/vttablet/tabletserver/tabletenv/config.go
@@ -124,6 +124,7 @@ func init() {
 	flag.StringVar(&currentConfig.TableACLExemptACL, "queryserver-config-acl-exempt-acl", defaultConfig.TableACLExemptACL, "an acl that exempt from table acl checking (this acl is free to access any vitess tables).")
 	flag.BoolVar(&currentConfig.TerseErrors, "queryserver-config-terse-errors", defaultConfig.TerseErrors, "prevent bind vars from escaping in returned errors")
 	flag.BoolVar(&currentConfig.AnnotateQueries, "queryserver-config-annotate-queries", defaultConfig.AnnotateQueries, "prefix queries to MySQL backend with comment indicating vtgate principal (user) and target tablet type")
+	flag.IntVar(&currentConfig.TruncateErrorLen, "queryserver-config-truncate-error-len", defaultConfig.TruncateErrorLen, "truncate errors if they are above this value (0 means do not truncate)")
 	flag.StringVar(&deprecatedPoolNamePrefix, "pool-name-prefix", "", "Deprecated")
 	flag.BoolVar(&currentConfig.WatchReplication, "watch_replication_stream", false, "When enabled, vttablet will stream the MySQL replication stream from the local server, and use it to update schema when it sees a DDL.")
 	flag.BoolVar(&currentConfig.TrackSchemaVersions, "track_schema_versions", false, "When enabled, vttablet will store versions of schemas at each position that a DDL is applied and allow retrieval of the schema corresponding to a position")
@@ -262,6 +263,7 @@ type TabletConfig struct {
 	WatchReplication                        bool    `json:"watchReplication,omitempty"`
 	TrackSchemaVersions                     bool    `json:"trackSchemaVersions,omitempty"`
 	TerseErrors                             bool    `json:"terseErrors,omitempty"`
+	TruncateErrorLen                        int     `json:"truncateErrorLen,omitempty"`
 	AnnotateQueries                         bool    `json:"annotateQueries,omitempty"`
 	MessagePostponeParallelism              int     `json:"messagePostponeParallelism,omitempty"`
 	CacheResultFields                       bool    `json:"cacheResultFields,omitempty"`
