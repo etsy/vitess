b455909501789532198eba4e61d18f68b6cc64e8
jchen <jchen@etsy.com>
2022-09-15 21:20:50 +0000

    backing out optimizations
    Signed-off-by: jchen <jchen@etsy.com>

--- go/vt/vtgate/vindexes/sqlite_lookup.go
+++ go/vt/vtgate/vindexes/sqlite_lookup.go
@@ -28,7 +28,7 @@ import (
 	"vitess.io/vitess/go/stats"
 	"vitess.io/vitess/go/vt/key"
 
-	sqlite3 "github.com/mattn/go-sqlite3" // sqlite driver
+	_ "github.com/mattn/go-sqlite3" // sqlite driver
 )
 
 const (
@@ -47,12 +47,6 @@ var (
 
 func init() {
 	Register("etsy_sqlite_lookup_unique", NewSqliteLookupUnique)
-	sql.Register("sqlite3_mmap_size", &sqlite3.SQLiteDriver{
-		ConnectHook: func(c *sqlite3.SQLiteConn) error {
-			_, err := c.Exec("pragma mmap_size = 30000000000", nil)
-			return err
-		},
-	})
 }
 
 // SqliteLookupUnique defines a vindex that uses a sqlite lookup table.
@@ -87,7 +81,7 @@ func NewSqliteLookupUnique(name string, m map[string]string) (Vindex, error) {
 	// TODO test cache=shared
 	dbDSN := "file:" + m["path"] + "?mode=ro&_query_only=true&immutable=true"
 	connectTime := time.Now()
-	db, err := sql.Open("sqlite3_mmap_size", dbDSN)
+	db, err := sql.Open("sqlite3", dbDSN)
 	if err != nil {
 		return nil, err
 	}
